539e21d9886934f1c03086c212c90dc0c5a089d3
Sam <31251023+sbleks@users.noreply.github.com>
2022-04-05 11:53:14 -0500

    Fix action

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcryptjs";
+// import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
 async function seed() {
-  const email = "rachel@remix.run";
+  // const email = "rachel@remix.run";
 
   // cleanup the existing database
   // await prisma.user.delete({ where: { email } }).catch(() => {
